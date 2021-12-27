ed0c1175a81cd6254f0d3c1849a554b1498d4f09
David Tabachnikov <nethunter@studiosh.com>
2021-12-27 21:57:53 +0100

    Add music and change navigation

--- src/index.js
+++ src/index.js
@@ -3,3 +3,37 @@ require('aframe-environment-component');
 require('aframe-extras');
 require('aframe-particle-system-component');
 require('super-hands');
+
+AFRAME.registerComponent('music-near-cake', {
+    init() {
+        this.el.addEventListener('navigation-end', event => {
+            if (event.detail.checkpoint.id === 'cake-checkpoint') this.onCakeCheckpoint();
+        });
+    },
+    onCakeCheckpoint() {
+        const el = document.querySelector('#cake');
+        console.log(el);
+        console.log(el.components);
+        el.components.sound.playSound();
+    }
+});
+
+AFRAME.registerComponent('change-color-on-hover', {
+    schema: {
+      color: {default: '#ceceff'}
+    },
+
+    init: function () {
+      var data = this.data;
+      var el = this.el;  // <a-box>
+      var defaultColor = el.getAttribute('material').color;
+
+      el.addEventListener('mouseenter', function () {
+        el.setAttribute('color', data.color);
+      });
+
+      el.addEventListener('mouseleave', function () {
+        el.setAttribute('color', defaultColor);
+      });
+    }
+  });
