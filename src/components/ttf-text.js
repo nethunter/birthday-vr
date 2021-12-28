de242234799df2653085614093fefa03930f1d4b
David Tabachnikov <nethunter@studiosh.com>
2021-12-28 09:18:32 +0100

    Add mouse and keyboard support

--- src/components/ttf-text.js
+++ src/components/ttf-text.js
@@ -5,32 +5,35 @@ import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
 AFRAME.registerComponent('ttf-text', {
     schema: {
         text: { type: 'string', default: 'Happy birthday!' },
+        font: { type: 'string', default: '/Poppins-Regular.ttf' },
         size: { type: 'number', default: 0.5 },
         height: { type: 'number', default: 0.2 },
-        color: { type: 'color', default: 'pink' }
+        color: { type: 'color', default: 'pink' },
+        bevel: { type: 'boolean', default: false },
+
     },
 
     /**
- * Initial creation and setting of the mesh.
- */
+     * Initial creation and setting of the mesh.
+     */
     init: function () {
         var data = this.data;
         var el = this.el;
 
-
         const text_loader = new TTFLoader();
-        text_loader.load("/Poppins-Regular.ttf", data => {
-            console.log("loaded", data)
-            const font = new FontLoader().parse(data)
-            this.geometry = new TextGeometry('Happy Birthday', {
+        text_loader.load(data.font, loaded => {
+            const font = new FontLoader().parse(loaded)
+
+            this.geometry = new TextGeometry(data.text, {
                 font: font,
-                size: 0.5,
-                height: 0.2,
+                size: data.size,
+                height: data.height,
                 bevelEnabled: true,
                 bevelThickness: 0.2,
                 bevelSize: 0.05,
 
             })
+
             this.geometry.center()
             this.material = new THREE.MeshStandardMaterial({
                 color: data.color,
